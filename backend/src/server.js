93e508d0b27fabc79f141be12f04c41614d3f051
MhmdHamoud702 <mohammadhammoud702@gmail.com>
2025-09-25 15:09:41 +0300

    Update commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,32 +5,37 @@ import authRoutes from "./routes/auth.route.js";
 import MessRoutes from "./routes/message.route.js";
 import { connectDb } from "./lib/db.js";
 import cors from "cors";
-import {app,server} from "./lib/socket.js"
+import { app, server } from "./lib/socket.js"; // use this app
 import path from "path";
 
 dotenv.config();
-
 const PORT = process.env.PORT || 5001;
 const ___dirname = path.resolve();
+
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
-if(process.env.NODE_ENV !== "production"){
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
 }
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",MessRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", MessRoutes);
 
-if(process.env.NODE_ENV === "production"){
-app.use(express.static(path.join(___dirname,"../frontend/dist")))
-app.get("/:path(.*)",(req,res)=>{
-    res.sendFile(path.join(___dirname,"../frontend","dist","index.html"))
-})}
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(___dirname, "../frontend/dist")));
+
+  // Catch-all route for React
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(___dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDb().then(() => {
   server.listen(PORT, () => {

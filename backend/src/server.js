269159c70afe2526a3b4546e95e280e62c53545a
MhmdHamoud702 <mohammadhammoud702@gmail.com>
2025-09-25 15:30:13 +0300

    Path 2  Update commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,8 @@ import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
-const ___dirname = path.resolve();
+const __dirname = path.resolve();
+
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
@@ -29,11 +30,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", MessRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(___dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendPath));
 
-  // Catch-all route for React
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(___dirname, "../frontend/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
